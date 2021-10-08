fc9e4b0ecbec9264c923f947ab5e107e3a087150
Andres Haro <hvaandres@icloud.com>
2021-10-07 23:16:52 -0600

    Initial Commit

---
+++ Pause/PauseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PauseApp.swift
+//  Pause
+//
+//  Created by Andres Haro on 10/7/21.
+//
+
+import SwiftUI
+
+@main
+struct PauseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
